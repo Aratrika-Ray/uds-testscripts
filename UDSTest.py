80f81bf2ffa490162ae1f890d11a8f09ddb997ff
Aratrika Ray <__aratrika.ray@ushur.com>
2021-12-15 12:39:25 +0530

    ap and sp working

--- UDSTest.py
+++ UDSTest.py
@@ -30,14 +30,17 @@ class run:
 
     # End of test
     def postTest(self, response):
+        global apDownload
         responseObject = json.loads(response)
         for assetsInfo in responseObject['assetsInfo']:
             for outputAssets in assetsInfo['outputAssets']:
                 # checking success status
                 if(outputAssets['status'] == "DATAEXTRACT_SUCCESS"):
                     self.s3.downloadFileAWS(outputAssets['extractedStructuredContent'])
-                    self.apDownload = True
-                else: print("\n There has been some error!", outputAssets['status'], "\n")
+                    apDownload = True
+                else:
+                    apDownload = False
+                    print("\n There has been some error!", outputAssets['status'], "\n")
         self.producer.close()
         self.consumer.close()
         print('********************** End of Test **********************')
@@ -48,26 +51,12 @@ class run:
         self.s3 = S3Utility.instance(self.config.getS3Region(), self.config.getS3Bucket())
         self.consumer = Consumer.newConsumer(self.config, self.onMessageReceived)
         self.producer = Producer.newProducer(self.config)
-        self.apDownload = False
         
-        # if(self.preTest(testId, description, uploadFilePath, rulesAssetPath[0], columnMappingPath)):
-        #     self.Test(testId)
-
-        # for both ap and sp rules
-        if(len(rulesAssetPath) > 1):
-            if(self.preTest(testId, description, uploadFilePath, rulesAssetPath[0], columnMappingPath)):
-                self.Test(testId, "aptrans_")
-            # after ap download, start sp process
-            self.consumer = Consumer.newConsumer(self.config, self.onMessageReceived)
-            self.producer = Producer.newProducer(self.config)
-            if(self.apDownload and self.preTest(testId, description, uploadFilePath, rulesAssetPath[1], columnMappingPath)):
-                self.Test(testId, "sptrans_")
-        else:
-            transformType = "aptrans_" if testId[len(testId)-2:]=="AP" else "sptrans_"
-            if(self.preTest(testId, description, uploadFilePath, rulesAssetPath[0], columnMappingPath)):
-                self.Test(testId, transformType)
+        transformType = "aptrans_" if testId[len(testId)-2:]=="AP" else "sptrans_"
+        
+        if(self.preTest(testId, description, uploadFilePath, rulesAssetPath, columnMappingPath)):
+            self.Test(testId, transformType)
             
-# run('Unit_Test','This is a template dry run', 'Scheme_605685_fortest.xlsx', 'rules.dslr', 'column-map1.json')
 # parser for command line flags
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", type=str, required=True)
@@ -80,13 +69,14 @@ args = parser.parse_args()
 # check for all rules combination    
 # ap & sp
 if(args.ap and args.sp): 
-    run('Unit_Test_AP_SP','This is a template dry run for ap and sp transform', args.f, [args.ap, args.sp], args.map)
+    run('Unit_Test_AP','This is a template dry run for ap transform', args.f, args.ap, args.map)
+    if(apDownload): run('Unit_Test_SP','This is a template dry run for sp transform', args.f, args.sp, args.map)
 #ap only
 elif(args.ap):
-    run('Unit_Test_AP','This is a template dry run for ap transform', args.f, [args.ap], args.map)
+    run('Unit_Test_AP','This is a template dry run for ap transform', args.f, args.ap, args.map)
 #sp only
 elif(args.sp):
-    run('Unit_Test_SP','This is a template dry run for sp transform', args.f, [args.sp], args.map)
+    run('Unit_Test_SP','This is a template dry run for sp transform', args.f, args.sp, args.map)
 # neither
 else: 
     print("Please use an AP or SP rule file to continue...")
