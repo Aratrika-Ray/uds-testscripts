bc7f09a2bac5ffdb18d7c3cf2869876218ad6a62
Ubuntu <ubuntu@ip-40-0-4-166.us-west-2.compute.internal>
2022-02-02 11:36:42 +0000

    temp

---
+++ SetUnitTest_1.py
@@ -0,0 +1,109 @@
+import os
+import sys
+from zipfile import ZipFile
+from UnitTestUDS import setTests
+from Regress import getrabbitmqconn
+
+class runTests:
+    topFolder = 'RegressionTests'
+    ap_rules = 'ap_rules.dslr'
+    ap_rules_new = 'ap_rules_new.dslr'
+    sp_rules = 'sp_rules.dslr'
+    sp_rules_new = 'sp_rules_new.dslr'
+    mapping = 'column-map1.json'
+
+    unitTests = len(os.listdir(topFolder))
+
+    def checkZip(self, folder):
+        return any(file.endswith('.zip') for file in os.listdir(folder))
+
+
+    def fileProcessing(self, folder, original_file, expected_files):
+        searchFolder = os.path.join(self.topFolder, folder)
+        fileIsDir = any(os.path.isdir(f"{searchFolder}/{file}") for file in expected_files)
+        filePath = f"{searchFolder}/{original_file}"
+
+        if(any('interleaved_' in file or 'new_' in file for file in expected_files)):
+            self.unitTestMap[folder]['properties']['uiparams'] = True
+           
+        if(not fileIsDir):            
+            ap = any('aptrans_' in file for file in expected_files)
+            sp = any('sptrans_' in file for file in expected_files)
+                
+            if(filePath not in self.unitTestMap[folder.split('/')[0]]['files']):
+                transformations = []
+                if(ap): transformations.append('aptrans')
+                if(sp): transformations.append('sptrans')
+                self.unitTestMap[folder.split('/')[0]]['files'][filePath] = transformations
+        else:
+            for file in expected_files:
+                expected_folder = os.listdir(f"{searchFolder}/{file}")
+            
+                ap = any('aptrans_' in file for file in expected_folder)
+                sp = any('sptrans_' in file for file in expected_folder)
+                    
+                if(filePath not in self.unitTestMap[folder.split('/')[0]]['files']):
+                    transformations = []
+                    if(ap): transformations.append('aptrans')
+                    if(sp): transformations.append('sptrans')
+                    self.unitTestMap[folder.split('/')[0]]['files'][filePath] = transformations   
+        
+
+    def unitFolderProcessing(self, original_folder, expected_folder):
+        originalFileFolder = os.path.join(self.topFolder, original_folder)
+        expectedFileFolder = os.path.join(self.topFolder, expected_folder)
+        original_files = [file for file in os.listdir(originalFileFolder) if file.startswith('original_')]
+
+        for original_file in original_files:
+            expected_files = [file for file in os.listdir(expectedFileFolder) if (file.startswith('expected_') and os.path.splitext(original_file.replace('original_',''))[0] in file)]
+            self.fileProcessing(original_folder, original_file, expected_files)
+
+
+    def topFolderProcessing(self, unitTestFolders):
+        for folder in unitTestFolders:
+            self.unitTestMap[folder] = {"properties": {"zipped": False, "uiparams": False}, "files": {}}
+            searchFolder = f"{self.topFolder}/{folder}"
+
+            if(self.checkZip(searchFolder)):
+                self.unitTestMap[folder]['properties']['zipped'] = True
+                    
+            self.unitFolderProcessing(folder, folder)
+
+#        for key in self.unitTestMap.keys():
+#            print(f"{key}: {self.unitTestMap[key]}")
+        oldClassifierTest, testRes = setTests(self.unitTestMap, self.ap_rules, self.sp_rules, self.mapping, self.topFolder) if self.topFolder == self.unitTestFolder else setTests({self.unitTestFolder: self.unitTestMap[self.unitTestFolder]}, self.ap_rules, self.sp_rules, self.mapping, self.topFolder)
+        if(oldClassifierTest):
+            print(f"\33[93mRegression Testing with Old Classifier Finished\nProceeding to Regression Testing with New Classifier\033[0;0m")
+            newClassifierTest, testRes = setTests(self.unitTestMap, self.ap_rules, self.sp_rules, self.mapping, self.topFolder, True) if self.topFolder == self.unitTestFolder else setTests({self.unitTestFolder: self.unitTestMap[self.unitTestFolder]}, self.ap_rules, self.sp_rules, self.mapping, self.topFolder, True)
+            if(newClassifierTest):
+                sys.stdout = sys.__stdout__
+                for key in testRes.keys():
+                    print(f"{key}: {testRes[key]}")
+
+                print(f"\n\033[1mRegression Testing Complete!\033[0m\n")
+
+
+    def __init__(self, regressionFolder):
+        folderBreakdown = regressionFolder.strip('/').split('/')
+        self.topFolder = folderBreakdown[0]
+        self.unitTestFolder = folderBreakdown[len(folderBreakdown)-1]
+        self.unitTestMap = {}
+
+        folders = [folder for folder in os.listdir(self.topFolder) if folder.startswith('Unit_Test_')]
+        self.topFolderProcessing(folders)
+
+
+def main():
+    try:
+        print("\033[1;31m\nUDS Regression Test Suite running. Please wait!\n\033[0;0m")
+        folderToRegress = sys.argv[1].strip('/')
+        runTests(folderToRegress)
+    except KeyboardInterrupt:
+        conns = getrabbitmqconn()
+        for conn in conns: 
+           conns[conn].close()
+        print("\033[1;31m\nPlease TERMINATE the session before running the Regression Test Suite again!\n\033[0;0m")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
--- UnitTestUDS.py
+++ UnitTestUDS.py
@@ -9,11 +9,7 @@ from fileChecking import errorChecking
 from Regress import setrabbitmqconn
 
 lock = {'folderLock': False, 'subFolderLock': False, 'downloadLock': False}
-# tests = {'Unit_Test_1': "Hidden Sheets - ", 'Unit_Test_2': "Multi-Header Columns AND Multiple File Transformation - ", 'Unit_Test_3': "Password Protected Files AND Wrong Password Case - ", 'Unit_Test_4': "Hidden Sheets AND File with Long Name - ", 'Unit_Test_5': "Zip Files AND Multiple Zip File Transformation- ", 'Unit_Test_6': "UI Params - ", 'Unit_Test_7': "Color Coding - ", 'Unit_Test_8': "Large Payloads - ", 'Unit_Test_9': ".xls and .xlsx Transformation - ", 'Unit_Test_10': "AP&SP Transformation - ",
-#     'NeuralNetClassifier_Unit_Test_1': "Hidden Sheets - ", 'NeuralNetClassifier_Unit_Test_2': "Multi-Header Columns AND Multiple File Transformation - ", 'NeuralNetClassifier_Unit_Test_3': "Password Protected Files AND Wrong Password Case - ", 'NeuralNetClassifier_Unit_Test_4': "Hidden Sheets AND File with Long Name - ", 'NeuralNetClassifier_Unit_Test_5': "Zip Files AND Multiple Zip File Transformation- ", 'NeuralNetClassifier_Unit_Test_6': "UI Params - ", 'NeuralNetClassifier_Unit_Test_7': "Color Coding - ", 'NeuralNetClassifier_Unit_Test_8': "Large Payloads(currently not working)", 'NeuralNetClassifier_Unit_Test_9': ".xls and .xlsx Transformation - ", 'NeuralNetClassifier_Unit_Test_10': "AP&SP Transformation - "}
 
-def sysExit():
-    sys.exit(0)
 
 class run:
     topFolder = "RegressionTests"
@@ -37,6 +33,7 @@ class run:
     # Generate a MQ message and send to UDS
     def Test(self, testId, file, transform, docName="originalNameWithPrefix", docMode="interleaved"):
         MQTemplateFile = f"{self.idealFolder.replace('NeuralNetClassifier_', '')}/{(os.path.splitext(os.path.split(file)[1])[0]).replace('original_', '')}_RequestMessageTemplate.json"
+        print(file)
         msg = RequestMessage.new(MQTemplateFile, testId, self.config)
         msg.newAsset(self.fileAssetId, 'no', 'tika')
 
@@ -53,11 +50,11 @@ class run:
                     for outputAssets in assetsInfo['outputAssets']:
                         # checking success status
                         if(outputAssets['hasTransformedAsset'] != False):
-                            while(lock['downloadLock']): continue
-                            lock['downloadLock'] = True
+#                            while(lock['downloadLock']): continue
+#                            lock['downloadLock'] = True
                             filemetadata, downloaded = self.s3.downloadFileAWS(outputAssets['extractedStructuredContent'], self.downloadFolder)
-                            print(f"{downloaded}\n{filemetadata}")
-                            lock['downloadLock'] = False
+                            print(f"{downloaded}\n{filemetadata}\n")
+#                            lock['downloadLock'] = False
                         else:
                             if(outputAssets['extractedStructuredContent'] == 'NONE'):
                                 print(f"There was an error downloading one of the files in {self.testID}")
@@ -67,6 +64,7 @@ class run:
                     print("\n**** NO ASSETS RECIEVED! ****\n")
             except Exception as e:
                 print(f"File could not be downloaded becase error -- {e} occured!")
+                continue
 
         self.numTests -= 1
         self.closeChannels(self.numTests == 0)
@@ -91,9 +89,11 @@ class run:
     def regressionSuite(self):
         unitTest = self.testID
         print(f"**** Regression test for {unitTest} ****\n")
-        subFolder = self.idealFolder.split('/')[-1]
+
+        idealFolder = self.downloadFolder.replace('transformed_', 'expected_')
+        subFolder = idealFolder.split('/')[-1]
         self.tests[unitTest] += subFolder if 'Unit_Test_' not in subFolder else ""
-        self.tests[unitTest] += " \033[1mPass\033[0m " if (RegressionSuite.regressionTest(self.idealFolder, self.downloadFolder) and self.tests[unitTest] != "Fail") else " \033[1mFail\033[0m "
+        self.tests[unitTest] += " \033[1mPass\033[0m " if (RegressionSuite.regressionTest(idealFolder, self.downloadFolder) and self.tests[unitTest] != "Fail") else " \033[1mFail\033[0m "
 
     # set values for pretesing and testing
     def startTest(self, folderMap, rulesAssetPath, columnMappingPath):
@@ -101,24 +101,26 @@ class run:
         
         subFolder = {}
         for file in folderMap['files']:
-            folder = os.path.split(file)[0]
+            folder = f"{os.path.split(file)[0]}/{os.path.splitext(os.path.basename(file))[0]}" if folderMap['properties']['zipped'] else os.path.split(file)[0]
             if(not folder in subFolder): subFolder[folder] = [(file, folderMap['files'][file])]
             else: subFolder[folder].append((file, folderMap['files'][file]))
         self.subFolders = len(subFolder)
+        print("SUBFOLDERS: ", subFolder)
 
         for folder in subFolder:
             while(lock['subFolderLock']): continue
             self.numTests = 0
-            lock['subFolderLock'] = True
+            lock['subFolderLock'] = True; print("FOLDER: ",folder)
             self.downloadFolder = folder.replace('original_', 'transformed_') if not self.newClassifier else folder.replace("/Unit", "/NeuralNetClassifier_Unit").replace('original', 'transformed')
-            self.idealFolder = self.downloadFolder.replace('transformed_', 'expected_')
+            print("DOWNLOAD FOLDER: ", self.downloadFolder)
+
             for file in subFolder[folder]:
                 self.numTests += len(file[1])
 
             for file in subFolder[folder]:
                 for transformation in file[1]:
                     ruleFilePath = rulesAssetPath[0] if transformation == 'aptrans' else rulesAssetPath[1]
-
+                    print("RUN FILE:", file[0], ruleFilePath)
                     if(uiparams):
                         uicombinations = [('originalNameWithPrefix', f"interleaved_{transformation}_", 'interleaved'),('originalNameWithSuffix', f"_interleaved_{transformation}", 'interleaved'),('retainOriginalName', '', 'interleaved'),('newName', f"interleaved_{transformation}_newName_of_{os.path.splitext(os.path.split(file[0])[1])[0].replace('original_', '')}", 'interleaved'),
                         ('originalNameWithPrefix', f"new_{transformation}_", 'new'),('originalNameWithSuffix', f"_new_{transformation}", 'new'),('retainOriginalName', '', 'new'),('newName', f"new_{transformation}_newName_of_{os.path.splitext(os.path.split(file[0])[1])[0].replace('original_', '')}", 'new')]
@@ -136,7 +138,7 @@ class run:
         self.s3 = S3Utility.instance(self.config.getS3Region(), self.config.getS3Bucket())
         self.consumer = Consumer.newConsumer(self.config, self.onMessageReceived)
         self.producer = Producer.newProducer(self.config)
-        setrabbitmqconn(type(self.producer), self.consumer)
+        setrabbitmqconn(self.producer, self.consumer)
 
         self.newClassifier = new
         self.testID = testId if not self.newClassifier else f"NeuralNetClassifier_{testId}"
@@ -155,7 +157,7 @@ class run:
 
 
 def setTests(unitFolderMap, ap, sp, map, topFolder, new=False):
-    sys.stdout = open(f"regression_report_{datetime.date(datetime.now())}.txt", "a")
+#    sys.stdout = open(f"regression_report_{datetime.date(datetime.now())}.txt", "a")
 
     for unitTestFolder in unitFolderMap:
         while(lock['folderLock']): continue
